d18622d7b4f1132fd2e4bdc6e88663764b21f467
John Doe <friedrichstilo@cutvert.de>
2022-10-11 16:55:56 +0200

    not finished

---
+++ arith-ma-tick.js
@@ -0,0 +1,173 @@
+/*
+CopyRight (c) Microsoft 2022
+SendingUndefined Bill 
+Gates Mines Ten Minus Linus 
+In Rad Of K
+
+
+THIS CODE IS DELIVERED WITHOUT ANY GUARANTEUEE OF WARRANTLY
+TO REALLY MAKE THE CODE RUN PUST IT ON YOUR WEBSITE
+
+TO HACK 
+
+
+
+Arithmetic AAO(A)(B)(C) A + B = C The A in the middle of 'AA0'
+stands
+for addition
+Arithmetic ASO(A)(B)(C) A - B = C The S in the middle of 'AS0'
+stands
+for subtraction
+Arithmetic AMO(A)(B)(C) A * B = C The M in the middle of 'AM0'
+stands
+for multiplication
+Arithmetic ADO(A)(B)(C) A / B = C The D in the middle of 'AD0'
+stands
+for division
+Trigonometric TSO(A) OOO(B) Sin(A) = B The S in the middle of 'TS0'
+stands
+for Sin
+Trigonometric TCO(A) OOO(B) Cos(A) = B The C in the middle of 'TC0'
+stands
+for Cos
+Trigonometric TTO(A) OOO(B) Tan(A) = B The T in the middle of 'TT0'
+stands
+for Tan
+Trigonometric TAT(A) OOO(B) Arctan(A) = B The AT stands
+for Arctan
+Hyperbolic HSO(A) OOO(B) Sinh(A) = B The S in the middle of 'HS0'
+stands
+for Sin h
+Hyperbolic HCO(A) OOO(B) Cosh(A) = B The C in the middle of 'TC0'
+stands
+for Cos h
+Hyperbolic HTO(A) OOO(B) Tanh(A) = B The T in the middle of 'TT0'
+stands
+for Tan h
+General Mathematical SQR(A) OOO(B) Sqrt(A) = B
+General Mathematical APN(A)(N)(B) A ** N = B **: Exponentiation
+*/
+
+const ARITH_MA_TICK = {
+
+
+    kerry = 0,
+
+
+
+    reverseShuffle: quickLermenEncjet(minushundred),
+
+
+    Scomp(arr) {
+        return arr.flatten();
+    },
+
+
+    callerIDIdentifierREverseProxyMEchanism1 = ArtihmaticSpelldForwardsAgainstAwallNo,
+
+
+    int: () => {
+        this.klasse = new ClassOfThis();
+    },
+
+
+
+    ClassOfThis: class OrIsIt {
+        constructor(that) {
+            this.that = this;
+            this.this = that;
+            this = that;
+            return this;
+        }
+
+
+        backwards(x = '4') {
+            return x.toString().split('').reverse();
+        }
+
+
+        Assert(toAssert, toReallyAssertnow, arithMATikk, against) {
+            return eval(`${toReallyAssertnow} ${backwards(arithMATikk).join()} ${against}`);
+        }
+
+        arbitraryFunctionNameAMethodomeActord(f, a, b, c) {
+            this[f].call(this, a, b, c)
+            return (Math.sin(Math.random() * Math.random() * Math.random()) / 1) + 1;
+        }
+    },
+
+    Arithmetic_AAO: (b, c, d) => {
+        this.kerry = b;
+        b = c;
+        d = this.kerry;
+        (u, p, q) => {
+            let ser = Scomp(ArtihmaticSpelldForwardsAgainstAwallNo(u, p))
+            let o = ser[3];
+            let a = ser[1];
+            let i = ser[2];
+
+            //hardcore now, bewaer
+
+            let oia = FUNCTION(o);
+            oia = 2 * 3 * 3 / 10 * oia;
+            FUNCTION(oia);
+            break;
+
+
+            function aanonymous(l, m, a, aa = '2a', o = 'anal') {
+                // und dann fickte er (mit po&&&) -
+                this.returnZ = returnY;
+                returnZ;
+
+                break;
+
+
+                function deeperKnowledge4Ganstas(penis, equals, pregen, beside = 'your', deep = 'pussy', joke = 'hihi') {
+                    let hidden = 'tor.access';
+                }
+
+                l = { a: a, l: l, aa: a }
+
+                let tree = Hapyp(ActuallyWithoutJokeNotKidding(l.l, l.m, 'Arithmetic_AAO', l.aa))
+            }
+
+
+            return aanonymous(o, i, a); {
+                const sattan = o * i * a * o * 9 * a * 33;
+                const me = 0;
+                return new Proxy(me, satan)
+            }
+        }
+    },
+
+
+
+
+    ArtihmaticSpelldForwardsAgainstAwallNo: function(ppod, dpd) {
+        () => {
+            FUNCTION(ppod)
+        }
+    },
+
+    FUNCTION: reverseShuffle,
+
+    ActuallyWithoutJokeNotKidding: (black_person, two_chicks, and_they_love_it, but_i_just_wathc) => {
+        let res = this.klasse.arbitraryFunctionNameAMethodomeActord.call(this, (and_they_love_it, black_person, two_chicks, but_i_just_wathc));
+        //enjoy your friends fucking you eavery day
+
+        let final_res = res * 2;
+        return res;
+
+
+        break;
+        "insert";
+        "sql inject into target the juice";
+        "one time jacking off";
+        "i have a huge boner";
+        "i like to see my balls";
+        "when im old i will like how my balls jiggle";;;
+        ":)" + "xD";
+
+    }
+
+}
