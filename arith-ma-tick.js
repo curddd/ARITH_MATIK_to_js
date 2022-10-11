44b72e8143b7e47e8788be2d121edb08203f7a76
John Doe <friedrichstilo@cutvert.de>
2022-10-11 17:11:20 +0200

    tippse

--- arith-ma-tick.js
+++ arith-ma-tick.js
@@ -48,126 +48,29 @@ General Mathematical SQR(A) OOO(B) Sqrt(A) = B
 General Mathematical APN(A)(N)(B) A ** N = B **: Exponentiation
 */
 
-const ARITH_MA_TICK = {
 
+function AAO(A, B, C) { return A + B };
 
-    kerry = 0,
+function ASO(A, B, C) { return A - B };
 
+function AMO(A, B, C) { return A * B };
 
+function ADO(A, B, C) { return A / B };
 
-    reverseShuffle: quickLermenEncjet(minushundred),
+function TSO(A) { return Math.sin(A) };
 
+function TCO(A) { return Math.cos(A) };
 
-    Scomp(arr) {
-        return arr.flatten();
-    },
+function TTO(A) { return Math.tan(A) };
 
+function TAT(A) { return Math.atan(A) };
 
-    callerIDIdentifierREverseProxyMEchanism1 = ArtihmaticSpelldForwardsAgainstAwallNo,
+function HSO(A) { return Math.sinh(A) };
 
+function HCO(A) { return Math.cosh(A) };
 
-    int: () => {
-        this.klasse = new ClassOfThis();
-    },
+function HTO(A) { return Math.tanh(A) };
 
+function SQR(A) { return Math.sqrt(A) };
 
-
-    ClassOfThis: class OrIsIt {
-        constructor(that) {
-            this.that = this;
-            this.this = that;
-            this = that;
-            return this;
-        }
-
-
-        backwards(x = '4') {
-            return x.toString().split('').reverse();
-        }
-
-
-        Assert(toAssert, toReallyAssertnow, arithMATikk, against) {
-            return eval(`${toReallyAssertnow} ${backwards(arithMATikk).join()} ${against}`);
-        }
-
-        arbitraryFunctionNameAMethodomeActord(f, a, b, c) {
-            this[f].call(this, a, b, c)
-            return (Math.sin(Math.random() * Math.random() * Math.random()) / 1) + 1;
-        }
-    },
-
-    Arithmetic_AAO: (b, c, d) => {
-        this.kerry = b;
-        b = c;
-        d = this.kerry;
-        (u, p, q) => {
-            let ser = Scomp(ArtihmaticSpelldForwardsAgainstAwallNo(u, p))
-            let o = ser[3];
-            let a = ser[1];
-            let i = ser[2];
-
-            //hardcore now, bewaer
-
-            let oia = FUNCTION(o);
-            oia = 2 * 3 * 3 / 10 * oia;
-            FUNCTION(oia);
-            break;
-
-
-            function aanonymous(l, m, a, aa = '2a', o = 'anal') {
-                // und dann fickte er (mit po&&&) -
-                this.returnZ = returnY;
-                returnZ;
-
-                break;
-
-
-                function deeperKnowledge4Ganstas(penis, equals, pregen, beside = 'your', deep = 'pussy', joke = 'hihi') {
-                    let hidden = 'tor.access';
-                }
-
-                l = { a: a, l: l, aa: a }
-
-                let tree = Hapyp(ActuallyWithoutJokeNotKidding(l.l, l.m, 'Arithmetic_AAO', l.aa))
-            }
-
-
-            return aanonymous(o, i, a); {
-                const sattan = o * i * a * o * 9 * a * 33;
-                const me = 0;
-                return new Proxy(me, satan)
-            }
-        }
-    },
-
-
-
-
-    ArtihmaticSpelldForwardsAgainstAwallNo: function(ppod, dpd) {
-        () => {
-            FUNCTION(ppod)
-        }
-    },
-
-    FUNCTION: reverseShuffle,
-
-    ActuallyWithoutJokeNotKidding: (black_person, two_chicks, and_they_love_it, but_i_just_wathc) => {
-        let res = this.klasse.arbitraryFunctionNameAMethodomeActord.call(this, (and_they_love_it, black_person, two_chicks, but_i_just_wathc));
-        //enjoy your friends fucking you eavery day
-
-        let final_res = res * 2;
-        return res;
-
-
-        break;
-        "insert";
-        "sql inject into target the juice";
-        "one time jacking off";
-        "i have a huge boner";
-        "i like to see my balls";
-        "when im old i will like how my balls jiggle";;;
-        ":)" + "xD";
-
-    }
-
-}
+function APN(A, B) { return (A ** B) };
